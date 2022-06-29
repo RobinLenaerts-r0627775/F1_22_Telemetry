5062901b018b7b51fcac81c7fb3f7a6c4e3837a5
RobinLenaerts-r0627775 <robin_lenaerts2011@hotmail.com>
2022-06-29 17:19:35 +0200

    initial project

---
+++ f1Telemetry/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+UdpClient client = null;
+try
+{
+    client = new UdpClient(20777);
+    Console.WriteLine("Listening...");
+}
+catch (Exception ex)
+{
+    Debug.WriteLine(ex.Message);
+}
+while (true)
+{
+
+    IPEndPoint server = new IPEndPoint(IPAddress.Any, 20777);
+
+    byte[] packet = client.Receive(ref server);
+    Console.WriteLine(packet.Length);
+    if(packet.Length == 972)
+    {
+        var header = packet.SubArray(2, 2);
+        Console.WriteLine("first byte = " + BitConverter.ToInt16(header));
+        var laptime = packet.SubArray(24, 4);
+        //if (BitConverter.IsLittleEndian)
+        //    Array.Reverse(laptime);
+
+        uint i = BitConverter.ToUInt32(laptime, 0);
+        var time = TimeSpan.FromMilliseconds(i);
+        var timestring = "" + time.Minutes + ":" + time.Seconds + ":" +time.Milliseconds;
+        Console.WriteLine("laptime: {0}", timestring);
+    }
+}
+public static class Extensions
+{
+    public static T[] SubArray<T>(this T[] array, int offset, int length)
+    {
+        T[] result = new T[length];
+        Array.Copy(array, offset, result, 0, length);
+        return result;
+    }
+}
