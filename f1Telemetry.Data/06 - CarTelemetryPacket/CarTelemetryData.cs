0d01540a45968b9cb635decc19df095a8e6ffe97
RobinLenaerts-r0627775 <robin_lenaerts2011@hotmail.com>
2022-07-08 18:18:07 +0200

    project structuring  + CarTelemetry to be tested

---
+++ f1Telemetry.Data/06 - CarTelemetryPacket/CarTelemetryData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace f1Telemetry.Data
+{
+    public class CarTelemetryData
+    {
+        ushort m_speed;                    // Speed of car in kilometres per hour
+        float m_throttle;                 // Amount of throttle applied (0.0 to 1.0)
+        float m_steer;                    // Steering (-1.0 (full lock left) to 1.0 (full lock right))
+        float m_brake;                    // Amount of brake applied (0.0 to 1.0)
+        byte m_clutch;                   // Amount of clutch applied (0 to 100)
+        sbyte m_gear;                     // Gear selected (1-8, N=0, R=-1)
+        ushort m_engineRPM;                // Engine RPM
+        byte m_drs;                      // 0 = off, 1 = on
+        byte m_revLightsPercent;         // Rev lights indicator (percentage)
+        ushort m_revLightsBitValue;        // Rev lights (bit 0 = leftmost LED, bit 14 = rightmost LED)
+        ushort[] m_brakesTemperature = new ushort[4];     // Brakes temperature (celsius)
+        byte[] m_tyresSurfaceTemperature = new byte[4]; // Tyres surface temperature (celsius)
+        byte[] m_tyresInnerTemperature = new byte[4]; // Tyres inner temperature (celsius)
+        ushort m_engineTemperature;        // Engine temperature (celsius)
+        float[] m_tyresPressure = new float[4];         // Tyres pressure (PSI)
+        byte[] m_surfaceType = new byte[4];           // Driving surface, see appendices
+
+    }
+}
---
+++ f1Telemetry.Data/06 - CarTelemetryPacket/PacketCarTelemetryData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace f1Telemetry.Data
+{
+    public class PacketCarTelemetryData
+    {
+        PacketHeader m_header;        // Header
+
+        CarTelemetryData[] m_carTelemetryData = new CarTelemetryData[22];
+
+        byte m_mfdPanelIndex;       /* Index of MFD panel open - 255 = MFD closed
+                                     * Single player, race – 0 = Car setup, 1 = Pits
+                                     * 2 = Damage, 3 =  Engine, 4 = Temperatures
+                                     * May vary depending on game mode
+                                     */
+
+        byte m_mfdPanelIndexSecondaryPlayer;   // See above
+        sbyte m_suggestedGear;       // Suggested gear for the player (1-8)
+                                    // 0 if no gear suggested
+
+    }
+}
