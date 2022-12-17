c0e86b0dee88ff692ffa5c39b429676cd6a5c45d
RobinLenaerts-r0627775 <robin_lenaerts2011@hotmail.com>
2022-12-17 01:08:07 +0100

    merge

--- f1Telemetry.Data/04 - ParticipantsPacket/PacketParticipantsData.cs
+++ f1Telemetry.Data/04 - ParticipantsPacket/PacketParticipantsData.cs
@@ -1,34 +1,14 @@
-<<<<<<< HEAD
 ﻿namespace f1Telemetry.Data;
 public class PacketParticipantsData
 {
     readonly PacketHeader m_header; // Header
     readonly byte m_numActiveCars; // Number of active cars in the data – should match number of cars on HUD
     readonly ParticipantData[] m_participants = new ParticipantData[22];
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace f1Telemetry.Data;
-
-public class PacketParticipantsData
-{
-    PacketHeader m_header; // Header
-    byte m_numActiveCars; // Number of active cars in the data – should match number of cars on HUD
-    ParticipantData[] m_participants = new ParticipantData[22];
->>>>>>> e166f1603536c8cb140e45ca933c396d1c09d387
 
     public PacketParticipantsData(byte[] packet)
     {
         var reader = new BinaryReader(new MemoryStream(packet));
-<<<<<<< HEAD
         m_header = PacketHeader.FromArray(reader.ReadBytes(24));
-=======
-        var m_header = PacketHeader.FromArray(reader.ReadBytes(24));
->>>>>>> e166f1603536c8cb140e45ca933c396d1c09d387
 
         m_numActiveCars = reader.ReadByte();
         var bytes = reader.ReadBytes(56);
