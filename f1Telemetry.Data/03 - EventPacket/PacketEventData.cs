2512a66b963e4e0a7ef9995396e397942c10e7cf
Robin Lenaerts <contact@robinlenaerts.dev>
2022-12-20 19:36:29 +0100

    code cleanup

--- f1Telemetry.Data/03 - EventPacket/PacketEventData.cs
+++ f1Telemetry.Data/03 - EventPacket/PacketEventData.cs
@@ -1,72 +1,69 @@
-﻿namespace f1Telemetry.Data
+﻿namespace f1Telemetry.Data;
+
+public class PacketEventData
 {
-    public class PacketEventData
+    readonly PacketHeader m_header; // Header
+    readonly byte[] m_eventStringCode = new byte[4]; // Event string code, see below
+
+    readonly EventDataDetails m_eventDetails; // Event details - should be interpreted differently
+    // for each type
+
+    public PacketEventData(byte[] packet)
     {
-        readonly PacketHeader m_header; // Header
-        readonly byte[] m_eventStringCode = new byte[4]; // Event string code, see below
+        var reader = new BinaryReader(new MemoryStream(packet));
+        m_header = PacketHeader.FromArray(reader.ReadBytes(24));
 
-        readonly EventDataDetails m_eventDetails; // Event details - should be interpreted differently
-        // for each type
-
-        public PacketEventData(byte[] packet)
+        m_eventStringCode = reader.ReadBytes(4);
+        var eventStringCode = Encoding.ASCII.GetString(m_eventStringCode);
+        byte[] bytes = Array.Empty<byte>();
+        switch (eventStringCode)
         {
-            var reader = new BinaryReader(new MemoryStream(packet));
-            m_header = PacketHeader.FromArray(reader.ReadBytes(24));
-
-            m_eventStringCode = reader.ReadBytes(4);
-            var eventStringCode = Encoding.ASCII.GetString(m_eventStringCode);
-            byte[] bytes = Array.Empty<byte>();
-            switch (eventStringCode)
-            {
-                case "SSTA":
-                    break;
-                case "SEND":
-                    break;
-                case "FTLP":
-                    bytes = reader.ReadBytes(5);
-                    break;
-                case "RTMT":
-                    bytes = reader.ReadBytes(1);
-                    break;
-                case "DRSE":
-                    break;
-                case "DRSD":
-                    break;
-                case "TMPT":
-                    bytes = reader.ReadBytes(1);
-                    break;
-                case "CHQF":
-                    break;
-                case "RCWN":
-                    bytes = reader.ReadBytes(1);
-                    break;
-                case "PENA":
-                    bytes = reader.ReadBytes(7);
-                    break;
-                case "SPTP":
-                    bytes = reader.ReadBytes(12);
-                    break;
-                case "STLG":
-                    bytes = reader.ReadBytes(1);
-                    break;
-                case "LGOT":
-                    break;
-                case "DTSV":
-                    bytes = reader.ReadBytes(1);
-                    break;
-                case "SGSV":
-                    bytes = reader.ReadBytes(1);
-                    break;
-                case "FLBK":
-                    bytes = reader.ReadBytes(8);
-                    break;
-                case "BUTN":
-                    bytes = reader.ReadBytes(4);
-                    break;
-            }
-            m_eventDetails = EventDataDetails.FromArray(bytes, eventStringCode);
+            case "SSTA":
+                break;
+            case "SEND":
+                break;
+            case "FTLP":
+                bytes = reader.ReadBytes(5);
+                break;
+            case "RTMT":
+                bytes = reader.ReadBytes(1);
+                break;
+            case "DRSE":
+                break;
+            case "DRSD":
+                break;
+            case "TMPT":
+                bytes = reader.ReadBytes(1);
+                break;
+            case "CHQF":
+                break;
+            case "RCWN":
+                bytes = reader.ReadBytes(1);
+                break;
+            case "PENA":
+                bytes = reader.ReadBytes(7);
+                break;
+            case "SPTP":
+                bytes = reader.ReadBytes(12);
+                break;
+            case "STLG":
+                bytes = reader.ReadBytes(1);
+                break;
+            case "LGOT":
+                break;
+            case "DTSV":
+                bytes = reader.ReadBytes(1);
+                break;
+            case "SGSV":
+                bytes = reader.ReadBytes(1);
+                break;
+            case "FLBK":
+                bytes = reader.ReadBytes(8);
+                break;
+            case "BUTN":
+                bytes = reader.ReadBytes(4);
+                break;
         }
-
         m_eventDetails = EventDataDetails.FromArray(bytes, eventStringCode);
     }
 }
